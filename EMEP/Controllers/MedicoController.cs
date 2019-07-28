358fe6eb250517853363cc5cff8505897c31fdba
Nairba <adrianmoova@gmail.com>
2019-07-28 13:07:13 -0600

    IndexMe

--- EMEP/Controllers/MedicoController.cs
+++ EMEP/Controllers/MedicoController.cs
@@ -37,7 +37,9 @@ namespace EMEP.Controllers
         {
 
             string correoSesion = Session["CorreoId"].ToString();
-            var medicos = db.Medico.Include(m => m.Tipo_Usuario);
+            var medicos = from m in db.Medico
+                          where m.correo == correoSesion
+                          select m;
 
             foreach (var medico in medicos)
             {
